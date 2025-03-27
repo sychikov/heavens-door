5da7581f479ba4216a5de64849cc6979a41ba925
sychikov <passiveincubus@gmail.com>
2025-03-27 11:01:23 +0000

    Add function for anathem users after reboot

--- scripts/module_anathem_user.py
+++ scripts/module_anathem_user.py
@@ -12,7 +12,7 @@ def check_status(tg):
 
 def change_status(tg):
 
-        result = module_database.change("update users set status = \"" + str(statuses.FlagAnathemed) + "\" where tg = "+str(tg)) 
+        result = module_database.change("update users set status = \"" + str(statuses.FlagAnathemed) + "\" where tg = "+str(tg))
 
 #Get ip from telergram id account
 def get_ip(tg):
@@ -22,7 +22,7 @@ def get_ip(tg):
                 return(list_ip)
 
         except:
-#               print("Database error")
+
                 sys.exit()
 
 #Use ip for block user by ip
@@ -30,20 +30,37 @@ def block_user(list_ip):
         try:
                 for ip in list_ip:
                         subprocess.check_output("iptables -A FORWARD -s " + ip + " -j REJECT", shell=True)
-                        #print("iptables -A FORWARD -s", ip, "-j REJECT")
         except:
-#               print("Iptables error")
                 sys.exit()
 #Return successful code
 
 def by_tg(tg):
-        #print(statuses.FlagAnathemed)
-        if str(check_status(tg)[0]) == str(statuses.FlagNotBlocked):
-                #print(statuses.FlagAnathemed)
+        if (str(check_status(tg)[0]) == str(statuses.FlagNotBlocked)) or (str(check_status(tg)[0]) == str(statuses.FlagBlocked)):
                 block_user(get_ip(tg))
                 change_status(tg)
-#       print("User has been blocked")
                 return 0
         else:
-#       print("User cannot be blocked, because he is already blocked")
                 return 1
+
+#If user was anathemed before restart server, iptables lose his rules
+
+#Anathem user by tg if it was anathemed
+def anathem_if_was_blocked():
+        ips_db = get_ips_anathemed_from_db()
+        if ips_db != '':
+                ips_iptables = get_anathemed_ips_from_iptables()
+                for ip in ips_db:
+                        if ip not in ips_iptables:
+                                anathem_single_ip(ip)
+                return 0
+        else:
+                return 1
+
+def get_ips_anathemed_from_db():
+        return module_database.get("select ip from users where status == " + str(statuses.FlagAnathemed))
+
+def get_anathemed_ips_from_iptables():
+        return subprocess.getoutput(["iptables -L | grep REJECT | awk '{print $4}'"])
+
+def anathem_single_ip(ip):
+        subprocess.check_output("iptables -A FORWARD -s " + ip + " -j REJECT", shell=True)
